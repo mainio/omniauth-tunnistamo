c841328687549f3ef5530a6fd2f1c3589563f5bc
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-03-31 19:01:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,116 @@
+# OmniAuth Tunnistamo (OpenID Connect)
+
+This is an unofficial OmniAuth strategy for authenticating with the Tunnistamo
+identity provider used by cities such as Helsinki and Turku. This gem is mostly
+a configuration wrapper around
+[`omniauth_openid_connect-saml`](https://github.com/m0n9oose/omniauth_openid_connect)
+which uses [`openid_connect`](https://github.com/nov/openid_connect) for OpenID
+Connect authentication implementation with identity providers, such as
+Tunnistamo.
+
+The gem can be used to hook Ruby/Rails applications to the Tunnistamo
+identification service.
+
+The gem has been developed by [Mainio Tech](https://www.mainiotech.fi/).
+
+The development has been sponsored by the
+[City of Turku](https://www.turku.fi/) and the
+[City of Helsinki](https://www.hel.fi/).
+
+The Tunnistamo service is originally developed by the City of Helsinki and it
+has been taken into use in other cities as well. You can find the original
+source code from:
+https://github.com/City-of-Helsinki/tunnistamo
+
+## Preparation
+
+In order to start using the Tunnistamo authentication endpoints, you will need
+to ask the Tunnistamo administrator to create an application in Tunnistamo
+with the callback URLs of your own service. Please contact the Tunnistamo
+administrator for further information or read through the Tunnistamo
+documentation.
+
+The details that you need to send to the Tunnistamo administrator are similar to
+the following information (apply to your service's domain) for
+non-Rails+Devise applications:
+
+- Your application's name: Decidim for City
+- Callback URL: https://www.service.fi/auth/tunnistamo/callback
+
+### Rails and Devise
+
+When applying this gem to Rails and Devise, the URLs can also include a path
+prefix to separate the scope of the authentication requests. For example, if
+you are using a `:user` scope with Devise, the callback URL would look like
+following:
+
+https://www.service.fi/users/auth/tunnistamo/callback
+
+## Installation and Configuration
+
+This gem has been only tested and used with Rails applications using Devise, so
+this installation guide only covers that part. In case you are interested to
+learn how you can use this with other frameworks, please refer to the
+[`omniauth_openid_connect-saml`](https://github.com/m0n9oose/omniauth_openid_connect)
+documentation and apply it to your needs (changing the strategy name to
+`:tunnistamo` and strategy class to `OmniAuth::Strategies::Tunnistamo`).
+
+To install this gem, add the following to your Gemfile:
+
+```ruby
+gem 'omniauth-tunnistamo'
+```
+
+For configuring the strategy for Devise, add the following in your
+`config/initializers/devise.rb` file:
+
+```ruby
+Devise.setup do |config|
+  config.omniauth :tunnistamo,
+    issuer: "https://tunnistamo.service.fi/openid",
+    client_options: {
+      port: 443,
+      scheme: "https",
+      host: "tunnistamo.service.fi",
+      identifier: "client-id-for-tunnistamo-from-their-admin",
+      secret: "client-secret-for-tunnistamo-from-their-admin",
+      redirect_uri: "https://www.service.fi/users/auth/tunnistamo/callback"
+    },
+    post_logout_redirect_uri: "https://www.service.fi"
+end
+```
+
+## Identification Responses
+
+The user's data is transmitted from Tunnistamo in the OpenID Connect
+authentication response. This data will be available in the OmniAuth
+[extra hash](https://github.com/omniauth/omniauth/wiki/Auth-Hash-Schema#schema-10-and-later).
+
+In order to access the response data, you can fetch the OmniAuth extra has and
+the corresponding user data in the OmniAuth callback handler, e.g. in Rails
+Devise controllers as follows:
+
+```ruby
+def raw_info
+  raw_hash = request.env["omniauth.auth"]
+  extra_hash = raw_hash[:extra]
+
+  extra_hash[:raw_info]
+end
+```
+
+### Personal Information Transmitted From MPASSid
+
+The user's personal information transmitted from Tunnistamo can be found under
+the `:raw_info` key in the OmniAuth extra hash described above.
+
+This attributes hash will contain the keys described by the Tunnistamo
+documentation or administrator. The information is limited to the underlying
+authentication service and what kind of data it provides.
+
+If you need more information about this data, contact the Tunnistamo
+administrator.
+
+## License
+
+MIT, see [LICENSE](LICENSE).
---
+++ spec/omniauth/strategies/tunnistamo_spec.rb
@@ -0,0 +1,278 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+# The underlying OpenIDConnect stategy should already handle and test most of
+# the OpenID connect specific functionality. The aim of this test is to test the
+# Tunnistamo specific functionality within that strategy and the Tunnistamo
+# custom strategy methods. This should ensure our customizations and the basic
+# flows also work when the underling strategy is changed.
+describe OmniAuth::Strategies::Tunnistamo, type: :strategy do
+  include OmniAuth::Test::StrategyTestCase
+
+  let(:auth_hash) { last_request.env['omniauth.auth'] }
+  let(:strategy_options) do
+    {
+      issuer: "#{auth_server_uri}/openid",
+      client_options: {
+        port: auth_server_uri_secured ? 443 : 80,
+        scheme: auth_server_uri_secured ? 'https' : 'http',
+        host: 'tunnistamo.test.fi',
+        identifier: client_id,
+        secret: client_secret,
+        redirect_uri: 'https://www.service.fi/auth/tunnistamo/callback'
+      },
+      post_logout_redirect_uri: 'https://www.service.fi'
+    }
+  end
+  let(:auth_server_uri) { 'https://tunnistamo.test.fi' }
+  let(:auth_server_uri_secured) { auth_server_uri =~ %r{^https://} }
+  let(:client_id) { 'client_id' }
+  let(:client_secret) { 'client_secret' }
+  let(:strategy) { [OmniAuth::Strategies::Tunnistamo, strategy_options] }
+
+  before do
+    # Stub the openid configuration to return the locally stored metadata for
+    # easier testing. Otherwise an external HTTP request would be made when the
+    # OmniAuth strategy is configured AND there would need to be a Tunnistamo
+    # authentication server always running at that URL.
+    configuration_file = begin
+      if auth_server_uri_secured
+        'openid_configuration_secured'
+      else
+        'openid_configuration'
+      end
+    end
+    stub_request(
+      :get,
+      "#{auth_server_uri}/openid/.well-known/openid-configuration"
+    ).to_return(status: 200, body: File.new(
+      support_filepath(configuration_file)
+    ), headers: {})
+  end
+
+  describe 'GET /auth/tunnistamo' do
+    subject { get '/auth/tunnistamo' }
+
+    it 'should apply the local options' do
+      is_expected.to be_redirect
+      expect(subject.location).to match(%r{^#{auth_server_uri}/openid/authorize\?client_id=#{client_id}&nonce=\w{32}&redirect_uri=https%3A%2F%2Fwww.service.fi%2Fauth%2Ftunnistamo%2Fcallback&response_type=code&scope=openid\+email\+profile&state=\w{32}$})
+
+      instance = last_request.env['omniauth.strategy']
+
+      expect(instance.options[:name]).to eq(:tunnistamo)
+      expect(instance.options[:discovery]).to be(true)
+      expect(instance.options[:scope]).to eq(%i[openid email profile])
+
+      expect(instance.options[:client_options]).to include(
+        'identifier' => client_id,
+        'secret' => client_secret,
+        'scheme' => 'https',
+        'host' => 'tunnistamo.test.fi',
+        'port' => 443,
+        'redirect_uri' => 'https://www.service.fi/auth/tunnistamo/callback',
+        'authorization_endpoint' => 'https://tunnistamo.test.fi/openid/authorize',
+        'token_endpoint' => 'https://tunnistamo.test.fi/openid/token',
+        'userinfo_endpoint' => 'https://tunnistamo.test.fi/openid/userinfo',
+        'end_session_endpoint' => 'https://tunnistamo.test.fi/openid/end-session',
+        'jwks_uri' => 'https://tunnistamo.test.fi/openid/jwks'
+      )
+    end
+
+    context 'with insecure server URI' do
+      let(:auth_server_uri) { 'http://tunnistamo.test.fi' }
+
+      it 'should hit the production metadata URL' do
+        is_expected.to be_redirect
+        expect(subject.location).to match(%r{^#{auth_server_uri}/openid/authorize\?client_id=#{client_id}&nonce=\w{32}&redirect_uri=https%3A%2F%2Fwww.service.fi%2Fauth%2Ftunnistamo%2Fcallback&response_type=code&scope=openid\+email\+profile&state=\w{32}$})
+
+        instance = last_request.env['omniauth.strategy']
+
+        expect(instance.options[:name]).to eq(:tunnistamo)
+        expect(instance.options[:discovery]).to be(true)
+        expect(instance.options[:scope]).to eq(%i[openid email profile])
+
+        expect(instance.options[:client_options]).to include(
+          'identifier' => client_id,
+          'secret' => client_secret,
+          'scheme' => 'http',
+          'host' => 'tunnistamo.test.fi',
+          'port' => 80,
+          'redirect_uri' => 'https://www.service.fi/auth/tunnistamo/callback',
+          'authorization_endpoint' => 'http://tunnistamo.test.fi/openid/authorize',
+          'token_endpoint' => 'http://tunnistamo.test.fi/openid/token',
+          'userinfo_endpoint' => 'http://tunnistamo.test.fi/openid/userinfo',
+          'end_session_endpoint' => 'http://tunnistamo.test.fi/openid/end-session',
+          'jwks_uri' => 'http://tunnistamo.test.fi/openid/jwks'
+        )
+      end
+    end
+
+    context 'with lang parameter' do
+      let(:lang_parameter) { 'locale' }
+
+      shared_examples 'lang added' do |request_locale, expected_locale|
+        subject { get "/auth/tunnistamo?#{lang_parameter}=#{request_locale}" }
+
+        it do
+          is_expected.to be_redirect
+
+          location = URI.parse(last_response.location)
+          if expected_locale.nil?
+            expect(location.query).not_to match(/&lang=#{request_locale}&/)
+          else
+            expect(location.query).to match(/&lang=#{expected_locale}&/)
+          end
+        end
+      end
+
+      context 'when set to fi' do
+        it_behaves_like 'lang added', 'fi', 'fi'
+      end
+
+      context 'when set to fi-FI' do
+        it_behaves_like 'lang added', 'fi-FI', 'fi'
+      end
+
+      context 'when set to sv' do
+        it_behaves_like 'lang added', 'sv', 'sv'
+      end
+
+      context 'when set to sv_SE' do
+        it_behaves_like 'lang added', 'sv_SE', 'sv'
+      end
+
+      context 'when set to en_GB' do
+        it_behaves_like 'lang added', 'en_GB', 'en'
+      end
+
+      context 'when set to en-US' do
+        it_behaves_like 'lang added', 'en-US', 'en'
+      end
+
+      context 'when set to et' do
+        it_behaves_like 'lang added', 'et', nil
+      end
+
+      context 'when set to de-DE' do
+        it_behaves_like 'lang added', 'de-DE', nil
+      end
+
+      context 'when set to nb_NO' do
+        it_behaves_like 'lang added', 'nb_NO', nil
+      end
+
+      context 'with lang parameter set to "language"' do
+        let(:lang_parameter) { 'language' }
+
+        it_behaves_like 'lang added', 'fi', 'fi'
+      end
+
+      context 'with lang parameter set to "lang"' do
+        let(:lang_parameter) { 'lang' }
+
+        it_behaves_like 'lang added', 'fi', 'fi'
+      end
+    end
+  end
+
+  describe 'POST /auth/tunnistamo/callback' do
+    let(:code) { SecureRandom.hex(16) }
+    let(:state) { SecureRandom.hex(16) }
+    let(:nonce) { SecureRandom.hex(16) }
+    let(:sub) { '248289761001' }
+    let(:private_key) { OpenSSL::PKey::RSA.generate(2048) }
+    let(:public_key) { private_key.public_key }
+    let(:access_token) { 'test_access_token' }
+    let(:id_token) do
+      ::OpenIDConnect::ResponseObject::IdToken.new(
+        iss: 'http://tunnistamo.test.fi',
+        sub: sub,
+        aud: 's6BhdRkqt3',
+        nonce: nonce,
+        exp: 1_311_281_970,
+        iat: 1_311_280_970
+      )
+    end
+
+    before do
+      get '/'
+      session['omniauth.state'] = state
+      session['omniauth.nonce'] = nonce
+
+      access_token_json = {
+        access_token: access_token,
+        id_token: id_token,
+        token_type: 'Bearer'
+      }
+
+      stub_request(
+        :post,
+        "#{auth_server_uri}/openid/token"
+      ).with(
+        body: {
+          'code' => code,
+          'grant_type' => 'authorization_code',
+          'redirect_uri' => 'https://www.service.fi/auth/tunnistamo/callback',
+          'scope' => 'openid email profile'
+        }
+      ).to_return(
+        status: 200,
+        body: JSON.generate(access_token_json),
+        headers: {'Content-Type' => 'application/json'}
+      )
+
+      userinfo_json = {
+        sub: sub,
+        preferred_username: 'Johnny',
+        first_name: 'John Jimmy',
+        given_name: 'John',
+        middle_name: 'Jimmy',
+        family_name: 'Doe',
+        email: 'john.doe@foo.bar',
+        birthdate: '1975-12-31',
+        gender: 'male'
+      }
+
+      stub_request(
+        :get,
+        "#{auth_server_uri}/openid/userinfo"
+      ).with(
+        headers: {'Authorization' => "Bearer #{access_token}"}
+      ).to_return(
+        status: 200,
+        body: JSON.generate(userinfo_json),
+        headers: {'Content-Type' => 'application/json'}
+      )
+
+      # Finally, request the callback when the stubs are in place
+      get(
+        '/auth/tunnistamo/callback',
+        code: code,
+        state: state,
+        id_token: id_token.to_jwt(private_key)
+      )
+    end
+
+    it do
+      auth = last_request.env['omniauth.auth']
+
+      expect(auth[:info][:email]).to eq('john.doe@foo.bar')
+      expect(auth[:info][:nickname]).to eq('Johnny')
+      expect(auth[:info][:first_name]).to eq('John')
+      expect(auth[:info][:last_name]).to eq('Doe')
+      expect(auth[:info][:gender]).to eq('male')
+
+      expect(auth[:extra][:raw_info][:birthdate]).to eq('1975-12-31')
+    end
+  end
+
+  describe 'GET /auth/tunnistamo/logout' do
+    subject { get '/auth/tunnistamo/logout' }
+
+    it do
+      is_expected.to be_redirect
+      expect(subject.location).to match(%r{^#{auth_server_uri}/openid/end-session\?post_logout_redirect_uri=https%3A%2F%2Fwww.service.fi$})
+    end
+  end
+end
